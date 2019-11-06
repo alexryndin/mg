053976f2e47c9dfc4e7dda47ea2ce9da81e60a9d
Alex Ryndin <alexnryndin@gmail.com>
2019-11-06 12:30:44 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::env;
+use std::process;
+
+use mg::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Error occured: {}, abort", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = mg::run(config) {
+        println!("Error occured: {}, abort", e);
+        process::exit(1);
+    };
+
+}
