5cac2506c27215cd31a89dc3a13c88629244e5f6
Alex Ryndin <alexnryndin@gmail.com>
2019-11-07 13:42:07 +0300

    write errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Error occured: {}, abort", err);
+        eprintln!("Error occured: {}, abort", err);
         process::exit(1);
     });
 
     if let Err(e) = mg::run(config) {
-        println!("Error occured: {}, abort", e);
+        eprintln!("Error occured: {}, abort", e);
         process::exit(1);
     };
 }
